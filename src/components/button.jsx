3cfb53f4f47f5bcfe99890c93d0f97f6ef806024
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-05-07 21:02:54 +0530

    commiting all changes to repo

---
+++ src/components/button.jsx
@@ -0,0 +1,25 @@
+import ButtonSvg from "../assets/svg/ButtonSvg";
+
+const Button = ({ classname, href, onClick, children, px, white }) => {
+  const classes = `button relative inxline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${classname || ""}`;
+
+  const spanClasses = "relative z-10 ";
+  const renderButton = () => (
+    <button className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+  return href ? renderLink() : renderButton();
+};
+
+export default Button;
